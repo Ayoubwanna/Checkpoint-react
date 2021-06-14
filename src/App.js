620c341d00337f90f6d3dbc98b67da90dad2ba8e
ayoub wanna <ayoubwanna11@gmail.com>
2021-06-14 11:31:38 +0100

    firtst commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container" >
+        <h1> Hi there</h1>
+      </div>
+      <div className="container" >
+      <div className="jumbotron">
+        <h1> this is my first react app  </h1>
+        <p>lololololololololol</p>
+      </div>
+      </div>
+    </div >
+  );
+}
+
+export default App;
